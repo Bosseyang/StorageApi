cdeb02431e3597feee824b9119a5821ebefd11b0
Bosseyang <bosse_yang@hotmail.com>
2025-06-25 14:48:55 +0200

    Added ProductsController and StorageContext

---
+++ StorageApi/Data/StorageContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StorageApi.Models.Entities;
+
+namespace StorageApi.Data
+{
+    public class StorageContext : DbContext
+    {
+        public StorageContext (DbContextOptions<StorageContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Product { get; set; } = default!;
+    }
+}
