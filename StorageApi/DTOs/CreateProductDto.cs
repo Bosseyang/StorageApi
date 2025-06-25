a4dfc31bb3e461e684c60e275c346832830d5e00
Bosseyang <bosse_yang@hotmail.com>
2025-06-25 16:13:54 +0200

    Added CreateProductDto and ProductDto

---
+++ StorageApi/DTOs/CreateProductDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StorageApi.DTOs;
+
+public class CreateProductDto
+{
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    [Required]
+    public int Price { get; set; }
+    public string Category { get; set; } = string.Empty;
+    public string Shelf { get; set; } = string.Empty;
+
+    [Range(0, int.MaxValue)]
+    public int Count { get; set; }
+    public string Description { get; set; } = string.Empty;
+
+}
+
