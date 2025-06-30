7ea7bf71c88d2341dfb63bc7f85a81697bab1231
Bosseyang <bosse_yang@hotmail.com>
2025-06-30 15:22:57 +0200

    Updated migration

--- StorageApi/Migrations/20250630125810_ProductsUpdate.Designer.cs
+++ StorageApi/Migrations/20250630125810_ProductsUpdate.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StorageApi.Data;
 
@@ -10,9 +11,11 @@ using StorageApi.Data;
 namespace StorageApi.Migrations
 {
     [DbContext(typeof(StorageContext))]
-    partial class StorageContextModelSnapshot : ModelSnapshot
+    [Migration("20250630125810_ProductsUpdate")]
+    partial class ProductsUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
